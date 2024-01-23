154d15bab6e4ca55653eaa35509a39627517d759
gui vieira <guivieirasilva02@gmail.com>
2024-01-23 19:51:18 -0300

    estrutura e estilização da pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,70 +1,88 @@
 import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
 import Hero from '../../assets/images/hero.svg'
-import BgHero from '../../assets/images/hero-bg.svg'
+// import BgHero from '../../assets/images/hero-bg.svg'
 
-import { ContainerHero, HeroContent, Heading } from './styles'
+import {
+  ContainerHero,
+  HeroContent,
+  Heading,
+  ContainerHome,
+  CoffeeList,
+} from './styles'
 import { useTheme } from 'styled-components'
+import { CoffeeCard } from '../../components/CoffeeCard'
 
 export function Home() {
   const theme = useTheme()
   return (
-    <ContainerHero>
-      <HeroContent>
-        <Heading>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <span>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </span>
-        </Heading>
+    <ContainerHome>
+      <ContainerHero>
+        <HeroContent>
+          <Heading>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <span>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </span>
+          </Heading>
 
-        <div id="grids">
-          <div>
-            <ShoppingCart
-              size={32}
-              weight="fill"
-              color={theme.colors.background}
-              style={{ backgroundColor: theme.colors.yellow.dark }}
-            />
-            <span>Compra simples e segura</span>
+          <div id="grids">
+            <div>
+              <ShoppingCart
+                size={32}
+                weight="fill"
+                color={theme.colors.background}
+                style={{ backgroundColor: theme.colors.yellow.dark }}
+              />
+              <span>Compra simples e segura</span>
+            </div>
+            <div>
+              <Package
+                size={32}
+                weight="fill"
+                color={theme.colors.background}
+                style={{ backgroundColor: theme.colors['base-text'] }}
+              />
+              <span>Embalagem mantém o café intacto</span>
+            </div>
+            <div>
+              <Timer
+                size={32}
+                weight="fill"
+                color={theme.colors.background}
+                style={{ backgroundColor: theme.colors.yellow.medium }}
+              />
+              <span>Entrega rápida e rastreada</span>
+            </div>
+            <div>
+              <Coffee
+                size={32}
+                weight="fill"
+                color={theme.colors.background}
+                style={{ backgroundColor: theme.colors.purple.medium }}
+              />
+              <span>O café chega fresquinho até você</span>
+            </div>
           </div>
-          <div>
-            <Package
-              size={32}
-              weight="fill"
-              color={theme.colors.background}
-              style={{ backgroundColor: theme.colors['base-text'] }}
-            />
-            <span>Embalagem mantém o café intacto</span>
-          </div>
-          <div>
-            <Timer
-              size={32}
-              weight="fill"
-              color={theme.colors.background}
-              style={{ backgroundColor: theme.colors.yellow.medium }}
-            />
-            <span>Entrega rápida e rastreada</span>
-          </div>
-          <div>
-            <Coffee
-              size={32}
-              weight="fill"
-              color={theme.colors.background}
-              style={{ backgroundColor: theme.colors.purple.medium }}
-            />
-            <span>O café chega fresquinho até você</span>
-          </div>
+        </HeroContent>
+        <div>
+          <img src={Hero} alt="Embalagem de Café do Coffee Delivery" />
         </div>
-      </HeroContent>
-      <div>
-        <img
-          src={Hero}
-          alt="Embalagem de Café do Coffee Delivery"
-          style={{ width: '100%' }}
-        />
-      </div>
-      <img id="bg" src={BgHero} alt="" />
-    </ContainerHero>
+        {/* <img id="bg" src={BgHero} alt="" /> */}
+      </ContainerHero>
+      <CoffeeList>
+        <h2>Nossos cafés</h2>
+        <div>
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+        </div>
+      </CoffeeList>
+    </ContainerHome>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,13 +1,27 @@
 import styled from 'styled-components'
 import { mixins } from '../../styles/mixins'
 
+export const ContainerHome = styled.main`
+  width: 100%;
+  max-width: 1440px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* h2 {
+    align-self: flex-start;
+    ${mixins.fonts.titleL}
+    color: ${({ theme }) => theme.colors['base-title']}
+  } */
+`
+
 export const ContainerHero = styled.section`
   display: flex;
   width: 100%;
-  max-width: 1120px;
-  padding: 6rem 0.5rem;
+  padding: 4rem 0.5rem;
   align-items: center;
   justify-content: center;
+  position: relative;
 
   @media (max-width: 850px) {
     flex-direction: column;
@@ -16,14 +30,11 @@ export const ContainerHero = styled.section`
 
   img#bg {
     position: absolute;
-    top: 0;
-    left: 0;
-    max-height: 544px;
-    width: 100vw;
+    max-height: 100%;
+    width: 100%;
     object-fit: cover;
   }
 `
-
 export const HeroContent = styled.div`
   display: flex;
   align-items: flex-start;
@@ -56,7 +67,7 @@ export const HeroContent = styled.div`
 export const Heading = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+
   gap: 16px;
 
   > h1 {
@@ -70,8 +81,66 @@ export const Heading = styled.div`
   }
 
   @media (max-width: 850px) {
+    align-items: center;
     > h1 {
       text-align: center;
     }
+  }
+`
+
+export const CoffeeList = styled.section`
+  max-width: 1440px;
+  padding: 32px 20px 150px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 54px;
+
+  > h2 {
+    ${mixins.fonts.titleL}
+    color: ${({ theme }) => theme.colors['base-subtitle']}
+  }
+
+  > div {
+    width: 90%;
+    display: grid;
+
+    grid-template-columns: repeat(4, 1fr);
+    grid-row-gap: 40px;
+    grid-column-gap: 32px;
+    @media (max-width: 1200px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (max-width: 850px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: 600px) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+`
+
+export const ContainerCoffees = styled.section`
+  display: grid;
+
+  gap: 16px;
+  width: 90%;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+    justify-content: center;
+  }
+
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
   }
 `
