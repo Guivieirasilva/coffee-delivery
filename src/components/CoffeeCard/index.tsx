2bcf18bef877df2c36e8059831bef4528b71488e
gui vieira <guivieirasilva02@gmail.com>
2024-01-29 19:26:43 -0300

    modificando a estrutura para receber apenas o coffee como prop

--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -1,36 +1,47 @@
 import { CheckFat, ShoppingCart } from '@phosphor-icons/react'
 import {
-  CoffeeContainer,
   Tags,
   Title,
   Description,
   Control,
   Price,
   Order,
+  CoffeeContainer,
 } from './styles'
-import coffeeExample from '../../assets/coffees/expresso.svg'
+
 import { QuantityInput } from '../Form/QuantityInput'
 import { useState } from 'react'
 import { useTheme } from 'styled-components'
 
-export function CoffeeCard() {
+interface CoffeesCardProps {
+  coffee: {
+    title: string
+    description: string
+    tags: string[]
+    price: number
+    image: string
+  }
+}
+
+export function CoffeeCard({ coffee }: CoffeesCardProps) {
   const theme = useTheme()
   const [isItemAdded] = useState(false)
+  const { image, description, price, tags, title } = coffee
 
   return (
     <CoffeeContainer>
-      <img src={coffeeExample} alt="" />
+      <img src={image} alt={description} />
       <Tags>
-        <span>tradicional</span>
+        {tags.map((tag, idx) => (
+          <span key={idx}>{tag}</span>
+        ))}
       </Tags>
-      <Title>Expresso Tradicional</Title>
-      <Description>
-        O tradicional café feito com água quente e grãos moídos
-      </Description>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
       <Control>
         <Price>
           <span>R$</span>
-          <span>9,90</span>
+          <span>{price.toFixed(2)}</span>
         </Price>
         <Order>
           <QuantityInput
