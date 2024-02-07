31d2700e98d3fcb19272ed19fb65b1f66a1c58d3
gui vieira <guivieirasilva02@gmail.com>
2024-02-06 21:32:27 -0300

    adicionando type = button

--- src/components/Form/QuantityInput/index.tsx
+++ src/components/Form/QuantityInput/index.tsx
@@ -14,11 +14,11 @@ export function QuantityInput({
 }: Props) {
   return (
     <Container>
-      <button onClick={decrementQuantity}>
+      <button type="button" onClick={decrementQuantity}>
         <Minus size={14} />
       </button>
       <span>{quantity}</span>
-      <button onClick={incrementQuantity}>
+      <button type="button" onClick={incrementQuantity}>
         <Plus size={14} />
       </button>
     </Container>
